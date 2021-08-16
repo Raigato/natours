eebc9a2d423093c6aad0ab41dad6c73035f9fd8c
Raigato <gabriel.raymondou@gmail.com>
2021-08-16 21:17:47 +0200

    url correction

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,9 +5,7 @@ const stripe = Stripe("pk_test_BffBAlozAfz3eBkViSORIUfv");
 
 export const bookTour = async (tourId) => {
   try {
-    const session = await axios(
-      `http://localhost:5000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
